3af78b8e2029873338629c049baf437ca6b038f3
VSETTER6 <vilhelm.settergren@live.se>
2024-11-19 15:17:51 +0100

    Solution + folders & classes created

---
+++ MediatRDemo/DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> personModelList = new();
+
+        public DemoDataAccess()
+        {
+            personModelList.Add(new PersonModel { Id = 1, FirstName = "John", LastName = "Dough" });
+            personModelList.Add(new PersonModel { Id = 2, FirstName = "Sue", LastName = "Strom" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return personModelList;
+        }
+
+        public PersonModel InsertPeople(string firstName, string lastName)
+        {
+            PersonModel personModel = new() { FirstName = firstName, LastName = lastName };
+            personModel.Id = personModelList.Max(maxAmountOfPeople => maxAmountOfPeople.Id) + 1;
+            personModelList.Add(personModel);
+            return personModel;
+        }
+    }
+}
