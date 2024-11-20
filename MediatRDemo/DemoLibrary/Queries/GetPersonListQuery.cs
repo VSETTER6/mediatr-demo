551f0691a667aaf044ff7191ac0f9506d8bbe2a6
VSETTER6 <vilhelm.settergren@live.se>
2024-11-20 18:13:03 +0100

    38 min into to video

---
+++ MediatRDemo/DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Queries
+{
+    public record GetPersonListQuery() : IRequest<List<PersonModel>>;
+
+
+
+    // Ifall det varit en klass hade det sett ungefär likadant ut.
+    // : IRequest<List<PersonModel>>; visar vad vi retunerar.
+
+    //public class GetPersonListQuery : IRequest<List<PersonModel>>;
+    //{  
+    //}
+}
